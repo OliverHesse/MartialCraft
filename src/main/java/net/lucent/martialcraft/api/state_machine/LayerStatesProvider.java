d285188678bde33fa129465577fcd1c93203edac
Oliver <oliver.f.p.hesse@gmail.com>
2026-08-18 20:23:20 +0100

    removed license and readme

--- src/main/java/net/lucent/martialcraft/api/state_machine/LayerStatesProvider.java
+++
@@ -1,7 +0,0 @@
-package net.lucent.martialcraft.api.state_machine;
-
-/**
- * determines The Set of Available States for a layer
- */
-public interface LayerStatesProvider<T extends StateContext>{
-}
